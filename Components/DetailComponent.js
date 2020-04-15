4b095a2547c83078e606cdb20b275e37fdd402ac
TrungNguyen7997 <63641988+TrungNguyen7997@users.noreply.github.com>
2020-04-15 16:01:20 +0700

    Create DetailComponent.js

---
+++ Components/DetailComponent.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import Button from 'react-native-button';
+
+export default class DetailComponent extends Component{
+    render(){
+        return(
+            <View style={styles.detail}>
+                asdasdasds
+            </View>
+                
+        )
+    }
+}
+const styles = StyleSheet.create({
+    detail: {
+        flexDerection: 'Coulum',
+        backgoundColor: ' #87CEFA',
+        alignItem: ' center',
+        justifyConten:'center'
+    },
+
+})
